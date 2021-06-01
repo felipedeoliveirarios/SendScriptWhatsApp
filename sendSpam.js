8f398e94ded6f9831be4bf27cf33e584ce20cef2
Felipe Rios <felipedeoliveirarios@gmail.com>
2021-06-01 19:57:31 -0300

    Removido o uso de URL por razões de segurança. Melhorias no loop.
    O Whatsapp Web não permite o uso de requsições além das que ele mesmo faz. Sendo assim, nada de URL.
    O loop agora funciona e pára quando deveria.

--- sendSpam.js
+++ sendSpam.js
@@ -1,22 +1,17 @@
-function enviarScriptDaURL(url, loop){
-    let text;
-    fetch(url)
-    .then(response => response.text())
-    .then(response_text => {
-        enviarScript(response_text, loop);
-    });
-}
-
 function enviarScript(scriptText, loop){
     const lines = scriptText.split("\n"); 
-    
-    let loopCounter = loop;
-    
-    do{
-        let i = 0;       
+    let i = 0;
+	let loopCounter = 0;
 
         setInterval(() => { 
-            if(i >= lines.length) return
+            if(i >= lines.length){
+				loopCounter++;
+				if (loop > 0 && loopCounter >= loop){
+					return;
+				} else {
+					i = 0;
+				}
+            }
 
             if(lines[i].trim() != ''){ 
                 console.log(lines[i]); 
@@ -35,9 +30,6 @@ function enviarScript(scriptText, loop){
 
             }
             i++;
-        }, 250);
-        loopCounter--;
-    } while (loop < 0 || loopCounter > 0 );
+        }, 1500);
 }
 
-enviarScriptDaURL('https://raw.githubusercontent.com/felipedeoliveirarios/SendScriptWhatsApp/5cc76f9a98b7a8bcac51783ef8d3f1da1cd76073/cachorro-salsicha.txt', 15);
